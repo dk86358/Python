87d70462b7cb841bcdf78981706cc47797b992d4
dpsk2008 <dpskusa@gmail.com>
2026-01-07 17:20:41 -0500

    my python file

---
+++ greeting.py
@@ -0,0 +1,12 @@
+def greeting(name):
+    print("Welcome, " + name)
+
+greeting("Kay")
+greeting("Deep")
+
+def greeting(name, department):
+    print("Welcome, " + name)
+    print("You are part of " + department)
+
+greeting("Dil", "Tech support")
+greeting("Jake", "IT Hub")
