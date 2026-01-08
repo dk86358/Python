868c2ae64e964d55bc40c626ee2c690048d56760
dpsk2008 <dpskusa@gmail.com>
2026-01-08 14:37:45 -0500

    my python file

---
+++ lucky.py
@@ -0,0 +1,20 @@
+name = "kay"
+number = len(name) * 9
+
+print("Hello " + name + " Your lucky number is " + str(number))
+
+name = "cameron"
+number = len(name) * 9
+
+print("Hello " + name + " Your lucky number is " + str(number))
+
+#function
+def lucky_number(name):
+    number = len(name) * 9
+
+    print("Hello " + name + " Your lucky number is " + str(number))
+lucky_number("Deep")
+lucky_number("Geo")
+
+
+
