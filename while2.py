f52cf3093387fcf207c137df4ab06392d8c40abc
dpsk2008 <dpskusa@gmail.com>
2026-01-17 14:08:51 -0500

    my python file

---
+++ while2.py
@@ -0,0 +1,19 @@
+my_variable = 5
+while my_variable < 10:
+    print("Hey")
+    my_variable += 1
+
+#Example 2 
+
+x= 1
+sum = 0
+while x < 10:
+    sum = sum + x
+    x = x + 1
+
+product = 1
+while x < 10 :
+    product = product * x
+    x = x + 1
+
+print ( sum , product)
