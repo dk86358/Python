36b6e78ff0d8d04d16ea1cfb058a268838ebc0c7
dpsk2008 <dpskusa@gmail.com>
2026-01-19 15:08:35 -0500

    my python file

---
+++ forloop2.py
@@ -0,0 +1,19 @@
+for n in range (1,5,6):
+    print(n)
+
+#Example 2
+
+for n in range (0,11,2):
+    print(n)
+
+#Example 3
+
+for number in range (2,7+1):
+    print(number*3)
+
+# Example 4
+
+for x in range (2,-2,-1):
+    print(x)
+
+
