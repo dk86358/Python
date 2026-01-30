831235720ef7e97a0e514972f9cb168bce62895f
dpsk2008 <dpskusa@gmail.com>
2026-01-30 15:06:38 -0500

    my python file

---
+++ friends.py
@@ -0,0 +1,8 @@
+def greeting_friends(friends):
+    for friend in friends:
+        print("Hi " + friend)
+    
+greeting_friends(['Tylor','Jamal','Eli'])
+
+greeting_friends("Deep ")
+                  
