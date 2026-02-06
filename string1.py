fa80346fd71041e1bceabc51ec5f89842df9cb74
dpsk2008 <dpskusa@gmail.com>
2026-02-06 15:16:04 -0500

    my python file

---
+++ string1.py
@@ -0,0 +1,21 @@
+"Mountains".upper()
+"Mountains".lower()
+
+#EG 2
+
+answer = "YES"
+if answer.lower() == "yes":
+    print("User said yes")
+
+#eg 3
+
+" yes ".strip
+
+" yes ".lstrip
+
+" yes ".strip
+
+
+" ".join(["this", "is", "phrase", "joined", "by", "spaces"])
+
+"This is anaother example".split()
