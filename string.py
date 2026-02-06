160408f1a4fff5c469feed4d737ef84f5806377b
dpsk2008 <dpskusa@gmail.com>
2026-02-06 13:47:47 -0500

    my python file

---
+++ string.py
@@ -0,0 +1,28 @@
+# Example 1
+
+message = "A kong string with a sillu typo"
+
+new_message = message[0:2] + "l" +message[3:]
+
+print(new_message)
+
+#Example 2
+message = " this is a new message"
+print(message)
+message = "Add another message"
+print(message)
+
+#Example 3
+
+pets="Cats & Dogs"
+pets.index("&")
+pets.index("C")
+pets.index("Dog")
+pets.index("s")
+
+#Example 4
+pets="Cats & Dogs"
+"Dragons" in pets
+"Cats" in pets
+
+
