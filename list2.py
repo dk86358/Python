22ef69da425e91a1d06fe2976c2f07a2cf2c421d
dpsk2008 <dpskusa@gmail.com>
2026-02-09 20:36:46 -0500

    my python file

---
+++ list2.py
@@ -0,0 +1,18 @@
+fruits = ["Pinneaple" , "Banana" ,  "Apple" , "Melon"]
+fruits.append("Kiwi")
+print(fruits)
+
+fruits.insert(0,"Orange")
+print(fruits)
+
+fruits.insert(25,"peach")
+print(fruits)
+
+fruits.remove("Melon")
+print(fruits)
+
+fruits.pop(3)
+print(fruits)
+
+fruits[2] = "Strawberry"
+print(fruits)
